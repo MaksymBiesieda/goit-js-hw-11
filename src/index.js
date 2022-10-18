a3f007156267469ad3c37c70c6ede532b0775527
MaksymBiesieda <maxbeseda@gmail.com>
2022-10-18 14:43:46 +0300

    makes the task

---
+++ src/index.js
@@ -0,0 +1,96 @@
+import simpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import Notiflix from "notiflix";
+import ApiService from "./API-Service";
+
+const formEl = document.querySelector('.search-form');
+const galleryEl = document.querySelector('.gallery');
+const buttonLoadMoreEl = document.querySelector('.load-more');
+
+const picturesApiService = new ApiService();
+let picturesNumber = null;
+
+formEl.addEventListener('submit', onSearch);
+buttonLoadMoreEl.addEventListener('click', onLoadMoreButton);
+
+function onSearch(event) {
+    event.preventDefault();
+    clearGallary();
+    picturesApiService.resetPage();
+    refreshPicturesNumber();
+    picturesApiService.searchQuery = event.currentTarget.elements.searchQuery.value;
+    if (picturesApiService.searchQuery === '') {
+      Notiflix.Notify.info("Enter your request please!");
+            return;  
+    }
+    picturesApiService.fetchPictures().then(pictures => {
+        if (pictures.hits.length === 0) {
+            Notiflix.Notify.warning("Sorry, there are no images matching your search query. Please try again.");
+            return;   
+      }
+      Notiflix.Notify.info(`Hooray! We found ${pictures.totalHits} images.`);
+      const markup = pictures.hits.map(picture =>
+        createMarkup(picture)
+        ).join('');
+        addGallary(markup); 
+        const lightbox = new SimpleLightbox('.gallery a', {});
+        showButton();
+    });
+    event.currentTarget.reset()
+}
+function onLoadMoreButton() {
+  hideButton();
+  picturesApiService.incrementPage();
+  picturesApiService.fetchPictures().then(pictures => {
+    const totalHits = pictures.totalHits;
+    incrementPictureNumber(pictures.hits);
+    if (picturesNumber >= pictures.totalHits) {
+      hideButton();
+      Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+      return;
+    }
+        const markup = pictures.hits.map(picture =>
+            createMarkup(picture)
+        ).join('');
+        addGallary(markup); 
+        const lightbox = new SimpleLightbox('.gallery a', {});
+        lightbox.refresh();
+        showButton();
+    })
+}
+
+function createMarkup(picture) {
+  return `<div class="photo-card">
+  <a href="${picture.largeImageURL}"><img src="${picture.webformatURL}" alt="${picture.tags}" loading="lazy" /></a>
+  <div class="info">
+    <p class="info-item"><b>Likes</b><br>${picture.likes}</p>
+    <p class="info-item"><b>Views</b><br>${picture.views}</p>
+    <p class="info-item"><b>Comments</b><br>${picture.comments}</p>
+    <p class="info-item"><b>Downloads</b><br>${picture.downloads}</p>
+  </div>
+</div>`
+}
+
+function addGallary(markup) {
+  galleryEl.insertAdjacentHTML("beforeend", markup);
+}
+
+function clearGallary() {
+  galleryEl.innerHTML = '';
+}
+
+function showButton() {
+buttonLoadMoreEl.classList.remove('is-hidden');
+}
+
+function hideButton() {
+ buttonLoadMoreEl.classList.add('is-hidden');
+}
+
+function refreshPicturesNumber() {
+  picturesNumber = picturesApiService.perPage;
+}
+
+function incrementPictureNumber(object) {
+  picturesNumber += object.length;
+}
